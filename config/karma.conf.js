e6b7e26d7e7886ee1978344023714cdc657337d9
ZhihuiTian <tian.zhihui@intasect.co.jp>
2016-11-15 14:32:34 +0900

    change something for jenkins.

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -86,18 +86,18 @@ module.exports = function(config) {
       //'Chrome'
     ],
 
-    customLaunchers: {
-      ChromeTravisCi: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    },
+    // customLaunchers: {
+    //   ChromeTravisCi: {
+    //     base: 'Chrome',
+    //     flags: ['--no-sandbox']
+    //   }
+    // },
 
     /*
      * Continuous Integration mode
      * if true, Karma captures browsers, runs the tests and exits
      */
-    singleRun: false
+    singleRun: true
   };
 
   // if (process.env.TRAVIS){
